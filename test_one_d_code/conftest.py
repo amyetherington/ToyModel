1c4aad17bd8646dbb4e8b016cb3544df779d46c9
jammy2211 <james.w.nightingale@durham.ac.uk>
2020-05-14 15:37:47 +0100

    conftest

---
+++ test_one_d_code/conftest.py
@@ -0,0 +1,12 @@
+from os import path
+from autoconf import conf
+
+import pytest
+
+directory = path.dirname(path.realpath(__file__))
+
+@pytest.fixture(autouse=True)
+def set_config_path():
+    conf.instance = conf.Config(
+        path.join(directory, "config"), path.join(directory, "output")
+    )
